dfd7f011de1996cc7207a484a700f2c799d8e7b6
TszDick Li <60331305+bli013@users.noreply.github.com>
2020-02-18 09:15:54 -0800

    Revert "completed assessment"

--- code-challenges.js
+++ code-challenges.js
@@ -11,17 +11,7 @@ var num2 = 0
 
 var num3 = -7
 // Expected output: "-7 is not divisble by three"
-// Create a function called isThreeAFriend that takes in 1 arg and === 3%
-isThreeAFriend = (num) => {
-    if (num % 3 === 0){
-        return `${num} is divisible by three`
-    } else{
-        return `${num} is not divisible by three`
-    }
-}
-console.log(isThreeAFriend(num1))
-console.log(isThreeAFriend(num2))
-console.log(isThreeAFriend(num3))
+
 
 
 
@@ -30,24 +20,6 @@ console.log(isThreeAFriend(num3))
 
 var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
-
-//create a function called capIt that takes in an array of strings
-capIt = (array) => {
-
-    let outPutArray= []
-// using for loop to pull one word at a time
-    for(let i=0; i < array.length; i++){
-    // then to split the string into individual letters and put it in an array
-        let wordArr = array[i].split("")
-    // then use the uppercase method to capitalize index 0
-        wordArr[0]= wordArr[0].toUpperCase()
-        let wordJoin = wordArr.join("")
-
-        outPutArray.push(wordJoin)
-    }
-        return outPutArray
-}
-console.log(capIt(randomNouns))
 
 
 
@@ -58,16 +30,6 @@ console.log(capIt(randomNouns))
 var mixedDataArray = [true, 8, "hello", 90, -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 90, 107]
 
-// create a function called orgNumOnly
-orgNumOnly = (arr) => {
-// map method for finding num only (typeof method)
-    let orgArr= arr.filter(el => typeof el === "number" )
-// .sort(el => (a, b) {return a-b}) to organize numbers
-    return orgArr.sort((a, b)=> a-b)
-}
-
-console.log(orgNumOnly(mixedDataArray))
-
 
 
 
@@ -77,53 +39,26 @@ console.log(orgNumOnly(mixedDataArray))
 
 var vowelTester1 = "learn"
 // Expected output: 1
-var vowelTester2 = "throw"
+var vowelTester1 = "throw"
 // Expected output: 3
 
-// create a function called winningVowel that passes a str as an argument
-winningVowel = (str) => {
-  // use a for loop to run through the string
-  for (let i=0; i<str.length; i++){
-  // create a new variable to set to charAt method
-  let indOf = str.charAt(i);
-  //set condition for finding vowel and either return
-  if ("AEIOUaeiou".indexOf(indOf) != -1)
-    return str.indexOf(indOf);
-  }
-  return "no vowels.";
-}
 
-console.log(winningVowel(vowelTester1))
-console.log(winningVowel(vowelTester2))
+
+
 
 
 // --------------------5) Create a function that takes three arguments - two numbers and a mathematical operation (+, -, *, /) and performs the given calculation. If the input tries to divide by 0, return: "Can't divide by 0!"
-//create a function called favCalc with 3 args (num1,op,num2)
-favCalc = (num1,op1,num2) => {
-  if(op1 === "*")
-    return num1 * num2
-  else if
-    (op1 === "+")
-    return num1 + num2
-  else if
-    (op1 === "-")
-    return num1 - num2  
-  else if
-    (op1 === "/" && num2 !== 0)
-    return num1 / num2
-  else
-    return "Can't devide by 0!"
-}
+
 
 
 
 
 // Uncomment and use the following console logs to test your function
-console.log(favCalc(3, "*" ,9))
+// console.log(calculator(3, "*" 9))
 // Expected output: 27
 
-console.log(favCalc(16, "+" ,3))
+// console.log(calculator(16, "+" 3))
 // Expected output: 19
 
-console.log(favCalc(89, "/" ,0))
+// console.log(calculator(89, "/" 0))
 // Expected output: "Can't divide by 0!"
--- interview-questions.md
+++ interview-questions.md
@@ -4,62 +4,57 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What does CRUD stand for?
 
-  Your answer: create, read, update, delete
+  Your answer:
 
-  Researched answer: In computer programming, create, read, update, and delete (CRUD) are the four basic functions of persistent storage.
+  Researched answer:
 
 
 
 2. What are the 5 HTTP verbs?
 
-  Your answer: I would google 5 HTTP verbs
+  Your answer:
 
   Researched answer:
-The primary or most-commonly-used HTTP verbs (or methods, as they are properly called) are POST, GET, PUT, PATCH, and DELETE.
+
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to render "Hello World" on the page?
 
-  Your answer: import class Components, extend components, render, return, and export.
+  Your answer:
 
   Researched answer:
-Functional components are the most basic type of component in React. They are often referred to as “stateless” components or presentational components. You are unable to define state within functional components as there is no context for the JS this keyword in a function.
+
 
 
 4. What is JSX?
 
-  Your answer: JSX is used in react and takes an gtml page and allows javascript syntax and functionality.
+  Your answer:
 
   Researched answer:
-JSX is an XML/HTML-like syntax used by React that extends ECMAScript so that XML/HTML-like text can co-exist with JavaScript/React code
+
 
 
 5. What is the difference between React state and props?
 
-  Your answer: State lives in the app.js or the main file that has all the smart components and aloows changes, whereas props is used on other component files to call back on something from the smart file. aka state is the weather and prop is the screen we see on our phones.
+  Your answer:
 
   Researched answer:
-  In a React component, props are variables passed to it by its parent component. State on the other hand is still variables, but directly initialized and managed by the component
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer: I'm not quite familiar with hoisting but the word hoist means to hold up or push up so, i would say that hoisting in 'javascript would have to do with somthing being held or pushed up, but I would defenitinly need to google it or phone a friend for further knowledge.
+  Your answer:
 
   Researched answer:
-Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+
 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-- React lifecycle methods-
-the series of events that happen from the birth of a React component to its death.
-- API-
-the series of events that happen from the birth of a React component to its death.
-- event.preventDefault()-
-method tells the user agent that if the event does not get explicitly handled, its default action should not be taken as it normally would be.
-- DOM events-
-DOM (Document Object Model) events are actions that occurs as a result of the user action or as result of state change of the elements of a DOM tree
+- React lifecycle methods
+- API
+- event.preventDefault()
+- DOM events
